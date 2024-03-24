7b75d8239deb7c3162d59f91b92dacf0003b2cba
PatsFan12 <144451514+PatsFan12@users.noreply.github.com>
2024-03-24 21:27:35 +0100

    Add files via upload

---
+++ WEB/feladat02.js
@@ -0,0 +1,46 @@
+// let a = 1
+// let b = 2
+
+// let [c, d, e] = [1,2,3]
+
+// let {f, g} = {f:1, g:2}
+// let obj = {f:1, g:2}
+
+// obj.f = 1
+
+
+// f
+
+// obj.f
+// obj.g
+
+// g
+
+// g = 4
+// obj.g = 4
+
+// 
+/**/
+/** */
+
+/* */
+
+// 1. feladat
+// let a = 15
+// let b = 12
+// let c = a+b
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// 2. Feladat
+// let lista = [0,7,16,30,41]
+// console.log(lista);
+
+// 3. Feladat
+let lista = [0,7,16,30,41]
+
+// for (let index = lista.length-1; index >= 0; index--){
+//     console.log(lista[index]);
+// }
+console.log(lista.reverse());
